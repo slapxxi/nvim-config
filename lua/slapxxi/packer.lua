aeee57430f9bfcd69b794c0afea4d8ab2a60f42f
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-04-12 22:26:14 +0300

    Refactor packer configuration: remove unused plugin entries and clean up commented code for better organization

--- lua/slapxxi/packer.lua
+++ lua/slapxxi/packer.lua
@@ -51,51 +51,16 @@ return require("packer").startup(function(use)
 	use("nvim-treesitter/nvim-treesitter-context")
 	use("lewis6991/gitsigns.nvim")
 	use("stevearc/conform.nvim")
-	-- use("catgoose/nvim-colorizer.lua")
 	use("numToStr/Comment.nvim")
 	use("windwp/nvim-ts-autotag")
 	use("luckasRanarison/tailwind-tools.nvim")
 
-	-- AVANTE
-	-- Required plugins
 	use("stevearc/dressing.nvim")
 	use("MunifTanjim/nui.nvim")
 	use("MeanderingProgrammer/render-markdown.nvim")
 
-	-- Optional dependencies
 	use("nvim-tree/nvim-web-devicons") -- or use 'echasnovski/mini.icons'
 	use("HakonHarnes/img-clip.nvim")
-
-	-- Avante.nvim with build process
-	use({
-		"yetone/avante.nvim",
-		branch = "main",
-		run = "make",
-		config = function()
-			require("avante").setup({
-				opts = {
-					-- provider = "openai",
-					provider = "groq",
-					openai = {
-						endpoint = "https://api.openai.com/v1",
-						model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-						timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-						temperature = 0,
-						max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-						--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-					},
-					vendors = {
-						groq = {
-							__inherited_from = "openai",
-							api_key_name = "GROQ_API_KEY",
-							endpoint = "https://api.groq.com/openai/v1/",
-							model = "llama-3.1-70b-versatile",
-						},
-					},
-				},
-			})
-		end,
-	})
 
 	use({
 		"OlegGulevskyy/better-ts-errors.nvim",
@@ -103,12 +68,15 @@ return require("packer").startup(function(use)
 			{ "MunifTanjim/nui.nvim" },
 		},
 	})
-	--use("mattn/emmet-vim")
 
-	-- use 'nvim-tree/nvim-tree.lua'
+	-- use("nvim-tree/nvim-tree.lua")
+	-- use("stevearc/oil.nvim")
 
-	-- use 'stevearc/oil.nvim'
-
-	-- temp disable
-	-- use {'romgrk/nvim-treesitter-context', cond = function() return false end}
+	-- temp disable example
+	-- use({
+	-- 	"romgrk/nvim-treesitter-context",
+	-- 	cond = function()
+	-- 		return false
+	-- 	end,
+	-- })
 end)
