7101fe7377afbc6159857fd143139327c9f2c059
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-04-13 20:16:12 +0300

    Enhance telescope configuration: add key mapping for <M-p> to find files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<M-p>", builtin.find_files)
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
