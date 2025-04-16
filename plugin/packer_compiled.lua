c130d2830592aad6b0a4a72e71d414cd454c5656
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-04-16 17:32:15 +0300

    add packer to .gitignore

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/slava/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?.lua;/home/slava/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?/init.lua;/home/slava/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?.lua;/home/slava/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/slava/.cache/nvim/packer_hererocks/2.1.1741730670/lib/lua/5.1/?.so"
+local package_path_str = "/Users/slava/.cache/nvim/packer_hererocks/2.1.1744317938/share/lua/5.1/?.lua;/Users/slava/.cache/nvim/packer_hererocks/2.1.1744317938/share/lua/5.1/?/init.lua;/Users/slava/.cache/nvim/packer_hererocks/2.1.1744317938/lib/luarocks/rocks-5.1/?.lua;/Users/slava/.cache/nvim/packer_hererocks/2.1.1744317938/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/slava/.cache/nvim/packer_hererocks/2.1.1744317938/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,12 +76,12 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["Comment.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/Comment.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/Comment.nvim",
     url = "https://github.com/numToStr/Comment.nvim"
   },
   ["better-ts-errors.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/better-ts-errors.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/better-ts-errors.nvim",
     url = "https://github.com/OlegGulevskyy/better-ts-errors.nvim"
   },
   ["bufferline.nvim"] = {
@@ -89,173 +89,173 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/opt/bufferline.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/opt/bufferline.nvim",
     url = "https://github.com/akinsho/bufferline.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-cmdline"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
     url = "https://github.com/hrsh7th/cmp-cmdline"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   ["cmp-vsnip"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
     url = "https://github.com/hrsh7th/cmp-vsnip"
   },
   ["conform.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/conform.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/conform.nvim",
     url = "https://github.com/stevearc/conform.nvim"
   },
   ["copilot.lua"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/copilot.lua",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/copilot.lua",
     url = "https://github.com/zbirenbaum/copilot.lua"
   },
   ["dirbuf.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/dirbuf.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/dirbuf.nvim",
     url = "https://github.com/elihunter173/dirbuf.nvim"
   },
   ["dressing.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/dressing.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/dressing.nvim",
     url = "https://github.com/stevearc/dressing.nvim"
   },
   ["gitsigns.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   harpoon = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/harpoon",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/ThePrimeagen/harpoon"
   },
   ["img-clip.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/img-clip.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/img-clip.nvim",
     url = "https://github.com/HakonHarnes/img-clip.nvim"
   },
   ["lspkind-nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/lspkind-nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/lspkind-nvim",
     url = "https://github.com/onsails/lspkind-nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["marks.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/marks.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/marks.nvim",
     url = "https://github.com/chentoast/marks.nvim"
   },
   ["nui.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/nui.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/nui.nvim",
     url = "https://github.com/MunifTanjim/nui.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-treesitter-context"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
     url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
   },
   ["nvim-ts-autotag"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
     url = "https://github.com/windwp/nvim-ts-autotag"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   playground = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/playground",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/playground",
     url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["render-markdown.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/render-markdown.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/render-markdown.nvim",
     url = "https://github.com/MeanderingProgrammer/render-markdown.nvim"
   },
   ["tailwind-tools.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/tailwind-tools.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/tailwind-tools.nvim",
     url = "https://github.com/luckasRanarison/tailwind-tools.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   undotree = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/undotree",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-surround"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/vim-surround",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/vim-surround",
     url = "https://github.com/tpope/vim-surround"
   },
   ["vim-vsnip"] = {
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/vim-vsnip",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/vim-vsnip",
     url = "https://github.com/hrsh7th/vim-vsnip"
   },
   ["windsurf.nvim"] = {
-    config = { "\27LJ\2\n…\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\17virtual_text\1\0\1\17virtual_text\0\1\0\2\fenabled\2\29default_filetype_enabled\2\nsetup\fcodeium\frequire\0" },
+    config = { "\27LJ\2\n…\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\17virtual_text\1\0\1\17virtual_text\0\1\0\2\29default_filetype_enabled\2\fenabled\2\nsetup\fcodeium\frequire\0" },
     loaded = true,
-    path = "/home/slava/.local/share/nvim/site/pack/packer/start/windsurf.nvim",
+    path = "/Users/slava/.local/share/nvim/site/pack/packer/start/windsurf.nvim",
     url = "https://github.com/Exafunction/windsurf.nvim"
   }
 }
@@ -263,7 +263,7 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: windsurf.nvim
 time([[Config for windsurf.nvim]], true)
-try_loadstring("\27LJ\2\n…\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\17virtual_text\1\0\1\17virtual_text\0\1\0\2\fenabled\2\29default_filetype_enabled\2\nsetup\fcodeium\frequire\0", "config", "windsurf.nvim")
+try_loadstring("\27LJ\2\n…\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\17virtual_text\1\0\1\17virtual_text\0\1\0\2\29default_filetype_enabled\2\fenabled\2\nsetup\fcodeium\frequire\0", "config", "windsurf.nvim")
 time([[Config for windsurf.nvim]], false)
 -- Conditional loads
 time([[Conditional loading of bufferline.nvim]], true)
