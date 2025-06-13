f1e2b87bebb71b84302c313919f0572e366ca210
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-06-13 11:05:51 +0300

    Use Oil as a default file explorer

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -4,7 +4,7 @@ if true then
 	require("oil").setup({
 		-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
 		-- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
-		default_file_explorer = false,
+		default_file_explorer = true,
 		-- Id is automatically added at the beginning, and name at the end
 		-- See :help oil-columns
 		columns = {
