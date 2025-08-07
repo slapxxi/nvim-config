250057eab5c14b92d99b300217779c49435e5c2a
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-08-07 14:48:40 +0300

    Keybinds

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -19,7 +19,7 @@ vim.keymap.set("n", "<leader>fr", builtin.resume, {})
 
 vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
 vim.keymap.set("n", "<leader>fw", builtin.lsp_workspace_symbols, {})
-vim.keymap.set("n", "<leader>ff", builtin.lsp_references, {})
+vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
 vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
 
 vim.keymap.set("n", "<leader>fg", builtin.git_files)
@@ -44,8 +44,8 @@ require("telescope").setup({
 		},
 		mappings = {
 			n = {
-				["<M-b>"] = require("telescope.actions").preview_scrolling_up,
-				["<M-f>"] = require("telescope.actions").preview_scrolling_down,
+				-- ["<M-b>"] = require("telescope.actions").preview_scrolling_up,
+				-- ["<M-f>"] = require("telescope.actions").preview_scrolling_down,
 				["<M-d>"] = require("telescope.actions").delete_buffer,
 			},
 			i = {
