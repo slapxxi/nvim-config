e6c54d2f6b1e67e1cec4eda429a312d6b9e17d09
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-11-15 13:09:31 +0300

    Override some devicons

---
+++ after/plugin/devicons.lua
@@ -0,0 +1,19 @@
+require("nvim-web-devicons").setup({
+	override = {
+		go = {
+			icon = "󰟓",
+			color = "#00ADD8",
+			name = "go",
+		},
+		["go.mod"] = {
+			icon = "󰟓",
+			color = "#00ADD8",
+			name = "go",
+		},
+		["go.sum"] = {
+			icon = "󰟓",
+			color = "#00ADD8",
+			name = "go",
+		},
+	},
+})
