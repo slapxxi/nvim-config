ed5d6005a6a66474e9b59c685bd1cdd3f73b2500
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-12-24 11:52:55 +0300

    add new keymaps for portal

--- after/plugin/portal.lua
+++ after/plugin/portal.lua
@@ -1,4 +1,6 @@
 if true then
 	vim.keymap.set("n", "<leader>o", "<cmd>Portal jumplist backward<cr>")
 	vim.keymap.set("n", "<leader>i", "<cmd>Portal jumplist forward<cr>")
+	vim.keymap.set("n", "<leader><leader>o", "<cmd>Portal changelist backward<cr>")
+	vim.keymap.set("n", "<leader><leader>i", "<cmd>Portal changelist forward<cr>")
 end
