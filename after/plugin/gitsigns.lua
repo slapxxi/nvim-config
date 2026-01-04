f80960ac7cfee8676225ab0a75d746a955e52b70
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-01-04 14:15:28 +0300

    add maps for gitsigns

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,7 +1,12 @@
-require("gitsigns").setup({
-	signcolumn = true,
-	numhl = false,
-	linehl = false,
-	word_diff = false,
-	sign_priority = 10,
-})
+if true then
+	vim.keymap.set("n", "<leader>sp", ":Gitsigns prev_hunk<CR>")
+	vim.keymap.set("n", "<leader>sn", ":Gitsigns next_hunk<CR>")
+
+	require("gitsigns").setup({
+		signcolumn = true,
+		numhl = false,
+		linehl = false,
+		word_diff = false,
+		sign_priority = 10,
+	})
+end
