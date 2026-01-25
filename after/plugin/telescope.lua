a6d88725dcbb5d3b426dbef2770c7acbfc848fda
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-01-25 10:11:03 +0300

    change telescope maps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -33,7 +33,7 @@ vim.keymap.set("n", "<leader>fm", builtin.marks, {})
 vim.keymap.set("n", "<leader>fk", builtin.keymaps, {})
 
 vim.keymap.set("n", "<leader>fll", builtin.diagnostics, {})
-vim.keymap.set("n", "<leader>fld", builtin.lsp_document_symbols, {})
+vim.keymap.set("n", "<leader>fls", builtin.lsp_document_symbols, {})
 vim.keymap.set("n", "<leader>flw", builtin.lsp_workspace_symbols, {})
 vim.keymap.set("n", "<leader>flr", builtin.lsp_references, {})
 vim.keymap.set("n", "<leader>fld", builtin.diagnostics, {})
