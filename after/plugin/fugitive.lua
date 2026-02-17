12219996413bf916252ffb88b847d09023a3127c
slapxxi <sl.pavlutin@gmail.com>
2026-02-17 10:41:55 +0300

    add keymap

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "<leader>g", ":vertical G<CR>")
+vim.keymap.set("n", "<leader>G", ":G")
 vim.keymap.set("n", "<leader>D", ":Gvdiffsplit<CR>")
 
 vim.keymap.set("n", "<leader>~", ":Glcd<CR>", { desc = "lcd to git root" })
