22bd079f0908e8c45b3d237725b3d47083633e9f
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-19 15:55:31 +0300

    format

--- after/plugin/resession.lua
+++ after/plugin/resession.lua
@@ -1,14 +1,16 @@
-local resession = require("resession")
+if true then
+	local resession = require("resession")
 
-resession.setup({
-	autosave = {
-		enabled = false,
-		interval = 60,
-		notify = true,
-	},
-})
+	resession.setup({
+		autosave = {
+			enabled = false,
+			interval = 60,
+			notify = true,
+		},
+	})
 
--- Resession does NOTHING automagically, so we have to set up some keymaps
-vim.keymap.set("n", "<leader>ss", resession.save)
-vim.keymap.set("n", "<leader>sl", resession.load)
-vim.keymap.set("n", "<leader>sd", resession.delete)
+	-- Resession does NOTHING automagically, so we have to set up some keymaps
+	vim.keymap.set("n", "<leader>ss", resession.save)
+	vim.keymap.set("n", "<leader>sl", resession.load)
+	vim.keymap.set("n", "<leader>sd", resession.delete)
+end
