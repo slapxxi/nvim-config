09725e759a2a5b86de97989a5a15835b2cc01f18
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-21 09:54:25 +0300

    add diff commands

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,6 +1,9 @@
 vim.keymap.set("n", "<leader>g", ":vertical G<CR>")
 vim.keymap.set("n", "<leader>G", ":G")
-vim.keymap.set("n", "<leader>D", ":Gvdiffsplit<CR>")
+
+vim.keymap.set("n", "<leader>v", ":Gvdiffsplit<CR>")
+vim.keymap.set("n", "<leader>h", ":Ghdiffsplit<CR>")
+vim.keymap.set("n", "<leader>o", ":diffoff<CR><c-w>b<c-w>o")
 
 vim.keymap.set("n", "<leader>~", ":Glcd<CR>", { desc = "lcd to git root" })
 vim.keymap.set("n", "<leader><leader>~", ":Gcd<CR>", { desc = "cd to git root" })
